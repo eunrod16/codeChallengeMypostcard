cf76f94b49afbed7b369b4125195b49a01a8073e
eunrod16 <hildaeunicer@hotmail.com>
2020-10-01 19:56:32 -0600

    curl get prices

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="mypostcardIcon.png">
